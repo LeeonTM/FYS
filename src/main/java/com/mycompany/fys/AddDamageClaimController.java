e23c66513bfd38a6f1d39135ef34aeac977f6341
yannick <yannick@mbp-van-yannick>
2018-01-10 14:31:32 +0100

    Damage claim improvements

---
+++ src/main/java/com/mycompany/fys/AddDamageClaimController.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.fys;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author yannick
+ */
+public class AddDamageClaimController extends BaseController {
+
+    @FXML
+    private AnchorPane basePane;
+    @FXML
+    private JFXButton managerButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        if (BaseController.loggedInUser.getRoleId() == 2) {
+            managerButton.setVisible(true);
+        }  
+    }    
+
+        @FXML
+    private void handleSchadeClaim(ActionEvent event) throws IOException {
+        super.swapScene(event, "damageOverview.fxml");
+    }
+
+    @FXML
+    private void handleOverview(ActionEvent event) throws IOException {
+        super.swapScene(event, "Overview.fxml");
+    }
+
+    @FXML
+    private void handleSettings(ActionEvent event) throws IOException {
+        super.swapScene(event, "Instellingen.fxml");
+    }
+
+    @FXML
+    private void handleLogout(ActionEvent event) throws IOException {
+        super.swapScene(event, "Login.fxml");
+    }
+
+    @FXML
+    private void handleManagerOverview(ActionEvent event) throws IOException {
+        super.swapScene(event, "managerStats.fxml");
+    }
+    
+}
